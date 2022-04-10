56a623adca12614320a456774216cf6a62a2b0f0
hung002059 <nguyenhoanghung.dthcl@gmail.com>
2022-04-10 18:39:22 +0700

    upload

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ document.getElementById("btnLogin").onclick = function (){
     document.getElementById("outputUN").innerHTML = "Tên tài khoản là :" +(userName);
 }
 document.getElementById("enter").onclick = function(){
-    var day = document.getElementById("enterNumb").value;
-    num = parseInt(num);
+    var day = document.getElementById("enterNumb").value*1;
+    // num = parseInt(num);
     const Luong1Ngay = 100000;
     luongThang = day * Luong1Ngay;
-    var currentFormat = new Intl.NumberFormat(vn-VN);
-    document.getElementById("Result").innerHTML = "Lương "+(num)+" là: "+(currentFormat.format(luongThang));
+    var currentFormat = new Intl.NumberFormat("vn-VN");
+    // document.getElementById("Result").innerHTML = "Lương "+day+" là: "+(currentFormat.format(luongThang));
+    console.log("Lương"+currentFormat.format(luongThang));
 }
