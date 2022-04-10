832fcff7713756547cff3b19e9223a456d8a0776
hung002059 <nguyenhoanghung.dthcl@gmail.com>
2022-04-10 20:02:01 +0700

    upload

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-document.getElementById("btnLogin").onclick = function (){
+document.getElementById("btnLogin").onclick = function(){
     var userName = document.getElementById("txtUserName").value;
     var passWord = document.getElementById("txtPassWord").value;
-    document.getElementById("outputUN").innerHTML = "Tên tài khoản là :" +(userName);
+    document.getElementById("resultUserName").innerHTML ="UserName: "+userName;
+    document.getElementById("resultPassWord").innerHTML ="PassWord: "+passWord;
 }
 document.getElementById("enter").onclick = function(){
-    var day = document.getElementById("enterNumb").value*1;
-    // num = parseInt(num);
+    var day = document.getElementById("enterNumb").value;
+    day = parseInt(day);
     const Luong1Ngay = 100000;
-    luongThang = day * Luong1Ngay;
+    var luongThang = day * Luong1Ngay;
     var currentFormat = new Intl.NumberFormat("vn-VN");
-    // document.getElementById("Result").innerHTML = "Lương "+day+" là: "+(currentFormat.format(luongThang));
-    console.log("Lương"+currentFormat.format(luongThang));
+    document.getElementById("Result").innerHTML = "Lương "+day+" ngày làm là: "+(currentFormat.format(luongThang));
+}
+document.getElementById("enterNumb").onclick = function (){
+    var num1 = document.getElementById("num_1").value;
+    num1 = parseInt(num1);
+    var num2 = document.getElementById("num_2").value;
+    num2 = parseInt(num2);
+    var num3 = document.getElementById("num_3").value;
+    num3 = parseInt(num3);
+    var num4 = document.getElementById("num_4").value;
+    num4 = parseInt(num4);
+    var num5 = document.getElementById("num_5").value;
+    num5 = parseInt(num5);
+    var TBC = (num1 + num2 + num3 + num4 + num5)/5;
+    document.getElementById("resultNumb").innerHTML = "TBC là:"+TBC;
 }
